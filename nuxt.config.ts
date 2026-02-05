df5eddc78a05ec250228e71f13ec1acb966a15db
chun3699 <65011212037@msu.ac.th>
2026-02-05 14:06:21 +0700

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       // apiBase: 'http://10.33.4.47:3000/api' 
-      apiBase: 'http://localhost:3030/api' // ชี้ไปที่ Backend ของเรา
+      apiBase: 'http://10.33.4.47:3000/api' // ชี้ไปที่ Backend ของเรา
     }
   }
 
